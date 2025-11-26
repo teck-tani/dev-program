9b5ada9281266d3a1f5ae2736e6e8ca56accd717
kwondeukcheon <kweon82@naver.com>
2025-11-27 08:45:57 +0900

    Updates

--- _next/static/QaR-X6dhdBp13J2sAVZt5/_buildManifest.js
+++ _next/static/QaR-X6dhdBp13J2sAVZt5/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/dev-program//_next/:path+",
-        "destination": "/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
