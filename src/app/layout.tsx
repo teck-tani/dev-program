6e5358836b1f71a72d5071196e2156fe38e0bef8
tani\권득천 <kweon82@naver.com>
2026-01-03 23:07:49 +0900

    도메인 명시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
     description: "일상생활과 업무에 필요한 다양한 웹 도구 모음. 계산기, 시계, 이모지 등 필요한 도구를 한 곳에서 사용하세요. 모바일에서도 편리하게 이용 가능합니다.",
     keywords: "웹 도구, 온라인 계산기, 대한민국 시계, 이모지 모음, 실용적인 웹 도구, 생활 도구, 개발자 도구, 온라인 유틸리티",
     alternates: {
-        canonical: "/",
+        canonical: "https://teck-tani.com",
     },
     openGraph: {
         type: "website",
